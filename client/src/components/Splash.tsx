6597977a4f0458dd66fd8f428860f3400411571d
Kumar Piyush <kr.piyush888@gmail.com>
2023-07-26 22:50:28 +0530

    completed without Form

--- client/src/components/Splash.tsx
+++ client/src/components/Splash.tsx
@@ -9,29 +9,25 @@ function Splash() {
     const isMobile = useStore((state: any) => state.isMobile);
 
     return (
-        <div className= {`splash flex flex-col justify-center items-center pb-10 pt-36 gap-10` + (isMobile ? `h-auto` : `h-full`)}>
+        <div className= {`splash flex flex-col justify-center items-center pb-20 pt-36 gap-10` + (isMobile ? `h-auto` : `h-full`)}>
             <motion.div
-            className = 'text-center flex flex-col justify-center items-center'>
+            className = 'text-center flex flex-col items-center'>
                 <motion.h1
                 initial = {{opacity: 0, scale: 0, y: 100}}
                 animate = {{opacity: 1, scale: 1, y: 0}}
                 transition = {{duration: 0.6}}
-                className = { (isMobile ? `text-2xl` : `text-5xl` ) + ` font-semibold ` + (isDark ? `text-white` : `text-black`)}>Hi! I am</motion.h1>
+                className = { (isMobile ? `text-3xl` : `text-5xl` ) + ` font-semibold ` + (isDark ? `text-white` : `text-black`)}>Hi! I am</motion.h1>
                 <motion.h1
                 initial = {{opacity: 0, scale: 0, y: 150}}
                 animate = {{opacity: 1, scale: 1, y: 0}}
                 transition = {{duration: 0.6}}
-                className = { (isMobile ? `text-5xl` : `text-8xl` ) + ` font-thin mt-6 ` + (isDark ? `text-white` : `text-black`)}>Kumar Piyush</motion.h1>
-                <motion.h1
-                initial = {{opacity: 0, scale: 0, y: 150}}
-                animate = {{opacity: 1, scale: 1, y: 0}}
-                transition = {{duration: 0.6}}
-                className = { (isMobile ? `text-3xl` : `text-5xl` ) + ` font-thin mt-4 ` + (isDark ? `text-white` : `text-black`)}>(vocarista)</motion.h1>
+                className = { (isMobile ? `text-6xl` : `text-9xl` ) + ` font-thin mt-6 ` + (isDark ? `text-white` : `text-black`)}>Kumar Piyush</motion.h1>
+                
                 <motion.h1
                 initial = {{opacity: 0, scale: 0, y: 200}}
                 animate = {{opacity: 1, scale: 1, y: 0}}
                 transition = {{duration: 0.6}}
-                className = "mt-8 text-5xl font-thin bg-gradient-to-r from-purple-600 to-blue-600 text-transparent bg-clip-text">{ `{ Learner, Dreamer, Developer }` }</motion.h1>
+                className = {(isMobile ? 'text-4xl' : 'text-6xl') + ` ` + `mt-8 font-thin bg-gradient-to-r from-purple-600 to-blue-600 text-transparent bg-clip-text` }>{ `{ Learner, Dreamer, Developer }` }</motion.h1>
                 <Button text = {`Download CV`} url = {`https://api.main.vocarista.com/download`} />
                 <ScrollAnimation />
             </motion.div>
