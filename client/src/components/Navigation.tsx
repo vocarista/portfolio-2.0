3c3533088c58b01062445c63a29092daf23c434f
Kumar Piyush <kr.piyush888@gmail.com>
2023-07-26 19:23:01 +0530

    implemented theme picker

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { useStore } from '../store/store';
 import ThemePicker from './ThemePicker';
