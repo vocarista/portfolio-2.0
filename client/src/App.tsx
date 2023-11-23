dbd2476f3b000d0913df4c6c311785f2c5ef846d
Kumar Piyush <kr.piyush888@gmail.com>
2023-11-23 16:47:30 +0530

    maintenance component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,22 @@
 import { useEffect } from 'react'
 import { useStore } from './store/store'
-import Splash from './components/Splash'
-import Projects from './components/Projects'
-import Roles from './components/Roles'
-import Schools from './components/Schools'
-import Certs from './components/Certs'
-import Skills from './components/Skills'
-import Achieve from './components/Achieve'
-import About from './components/About'
-import Navigation from './components/Navigation'
-import Footer from './components/Footer'
-import NavList from './components/NavList'
+// import Splash from './components/Splash'
+// import Projects from './components/Projects'
+// import Roles from './components/Roles'
+// import Schools from './components/Schools'
+// import Certs from './components/Certs'
+// import Skills from './components/Skills'
+// import Achieve from './components/Achieve'
+// import About from './components/About'
+// import Navigation from './components/Navigation'
+// import Footer from './components/Footer'
+// import NavList from './components/NavList'
 import MaintenanceComponent from './components/MaintenanceComponent'
 
 function App() {
   const toggleView = useStore((state: any) => state.toggleView);
   const isDark = useStore((state: any) => state.isDark);
-  const showNavList = useStore((state: any) => state.showNavList);
+  // const showNavList = useStore((state: any) => state.showNavList);
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth < 1000) toggleView(true);
